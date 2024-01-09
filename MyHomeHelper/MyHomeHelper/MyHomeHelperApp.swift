bc9ea9026cd7dd6324e404504707695a2ce0843a
JotaGalera <javigalera94@gmail.com>
2024-01-09 14:01:40 +0100

    Creating empty project

---
+++ MyHomeHelper/MyHomeHelper/MyHomeHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHomeHelperApp.swift
+//  MyHomeHelper
+//
+//  Created by Javier Galera Garrido on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MyHomeHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
